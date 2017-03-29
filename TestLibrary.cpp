9ac1b9c08b6baf1ea3f8b514ec28771e64f5ba60
KaushikNathMIT <kauanath@gmail.com>
2017-03-30 01:58:24 +0530

    Project name changed and objectdetectcuda library now splitted into proper c++ and .h format

--- TestLibrary.cpp
+++ TestLibrary.cpp
@@ -3,12 +3,26 @@
 //
 
 #include "MainLibrary/ObjectDetectCuda.h"
+
+void objectDetect();
+
+void basicFilter();
+
 using namespace std;
 using namespace cv;
 using namespace cv::cuda;
 
 
 int main() {
+    //objectDetect();
+    basicFilter();
+}
+
+void basicFilter() {
+
+}
+
+void objectDetect() {
     string cascadefilename = "/home/kaushik/Downloads/frontalFace10/haarcascade_frontalface_alt.xml";
     new ObjectDetectCuda(cascadefilename);
 }
